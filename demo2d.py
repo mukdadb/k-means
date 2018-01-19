a7a398e402a12bafcd5fbd813c5989f0d9fb011f
Marcin Sawicki <marcin.z.sawicki@gmail.com>
2018-01-19 18:48:31 +0100

    Better randomization for dataset.

--- demo2d.py
+++ demo2d.py
@@ -5,9 +5,9 @@ from kmeans import kmeans
 
 COLORS = ['b', 'g', 'r', 'c', 'm', 'y', 'k', 'w']
 
-sample = [list(arr) for arr in 100 * np.random.random_sample((1000, 2))]
+sample = [list(arr) for arr in 1000000 * np.random.random_sample((100, 2))]
 
-result = kmeans(sample, 6)
+result = kmeans(sample, 7)
 
 for cluster, color in zip(result, COLORS):
     plt.plot([point[0] for point in cluster], [point[1] for point in cluster],
