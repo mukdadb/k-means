aea62e31c9d28f828f6fa9ff3d3df45fd13bd72d
Marcin Sawicki <marcin.z.sawicki@gmail.com>
2018-01-23 10:28:53 +0100

    Add 3D demo.

---
+++ demo3d.py
@@ -0,0 +1,26 @@
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+import numpy as np
+
+from kmeans import kmeans
+
+COLORS = ['b', 'g', 'r', 'c', 'm', 'y', 'k', 'w']
+
+sample = [list(arr) for arr in 100 * np.random.random_sample((30, 3))]
+
+result = kmeans(sample, 3)
+
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+
+for c, cluster in zip(COLORS, tuple(result)):
+    xs = [point[0] for point in cluster]
+    ys = [point[1] for point in cluster]
+    zs = [point[2] for point in cluster]
+    ax.scatter(xs, ys, zs, c=c)
+
+for angle in range(0, 360):
+    print('ok')
+    ax.view_init(30, angle)
+    plt.draw()
+    plt.pause(.001)
