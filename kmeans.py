fd6c2eeaa2b488a6b0c01ae48fce19c55efb6f56
Marcin Sawicki <marcin.z.sawicki@gmail.com>
2018-01-23 14:02:53 +0100

    Split kmeans() function to several smaller for readability.

--- kmeans.py
+++ kmeans.py
@@ -2,10 +2,8 @@ import numpy as np
 from scipy.spatial.distance import euclidean
 
 
-def kmeans(points, k):
-    """K-means clustering for given k. Returns array of groups."""
-    # Centroids initialization
-    c = [None] * k            # Centroids coords
+def _init_centroids(points, k):
+    c = [None] * k  # Centroids coords
     d = [None] * len(points)  # Points' distances from nearest centroid
     c[0] = points[np.random.choice(len(points))]
     for i in range(1, k):
@@ -15,20 +13,32 @@ def kmeans(points, k):
         psum = sum(p)
         p = [x / psum for x in p]
         c[i] = points[np.random.choice(len(points), p=p)]
+    return c
+
+
+def _update_centroids(points, centroids, assignment):
+    new_c = [None] * len(centroids)
+    for i, centroid in enumerate(centroids):
+        assigned_points = [points[j]
+                           for j, point in enumerate(points)
+                           if assignment[j] == i]
+        new_c[i] = list(np.array(assigned_points).mean(axis=0))
+    return new_c
+
+
+def kmeans(points, k):
+    """K-means clustering for given k. Returns array of groups."""
+    # Centroids initialization
+    c = _init_centroids(points, k)
     # Main part
     while True:
         # Assignment
         assignment = [None] * len(points)  # index: point i, value: centroid i
         for i, point in enumerate(points):
-            assignment[i] = c.index(min(c, key=lambda cntr: euclidean(point,
-                                                                      cntr)))
+            assignment[i] = c.index(min(c,
+                                    key=lambda cntr: euclidean(point, cntr)))
         # Centroid update
-        new_c = [None] * len(c)
-        for i, centroid in enumerate(c):
-            assigned_points = [points[j]
-                               for j, point in enumerate(points)
-                               if assignment[j] == i]
-            new_c[i] = list(np.array(assigned_points).mean(axis=0))
+        new_c = _update_centroids(points, c, assignment)
         # Stop condition
         if np.array_equal(np.array(c), np.array(new_c)):
             break
