a593969efa0c4c1053cb1388d63ed9270ea44e24
Marcin Sawicki <marcin.z.sawicki@gmail.com>
2018-01-24 11:44:45 +0100

    demo3d.py now shows both data before and after clustering.

--- demo3d.py
+++ demo3d.py
@@ -6,21 +6,29 @@ from kmeans import kmeans
 
 COLORS = ['b', 'g', 'r', 'c', 'm', 'y', 'k', 'w']
 
-sample = [list(arr) for arr in 100 * np.random.random_sample((100, 
-3))]
+mu, kappa = 2.5, 2.0
+sample = [list(arr) for arr in 100 * np.random.vonmises(mu, kappa, (30, 3))]
 
-result = kmeans(sample, 7)
+result = kmeans(sample, 6)
 
 fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
+bx = fig.add_subplot(111, projection='3d')
+plt.title('Raw data')
+
+fig2 = plt.figure()
+ax = fig2.add_subplot(111, projection='3d')
+plt.title('Clustered data')
 
 for c, cluster in zip(COLORS, tuple(result)):
     xs = [point[0] for point in cluster]
     ys = [point[1] for point in cluster]
     zs = [point[2] for point in cluster]
     ax.scatter(xs, ys, zs, c=c)
+    bx.scatter(xs, ys, zs, c='k')
 
-for angle in range(0, 360):
-    ax.view_init(30, angle)
-    plt.draw()
-    plt.pause(.001)
+while True:
+    for angle in range(0, 360):
+        ax.view_init(30, angle)
+        bx.view_init(30, angle)
+        plt.draw()
+        plt.pause(.001)
